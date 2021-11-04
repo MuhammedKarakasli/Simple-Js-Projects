b67dc73cf56540e11d9bd7d0759ada9de9267bdc
Muhammed Karakaşlı <90934901+MuhammedKarakasli@users.noreply.github.com>
2021-11-04 17:25:32 +0300

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+function palindrome(str) {
+    let polishedString =str.replace(/\W+|_/g, "").toLowerCase();
+    let reversedString = polishedString.split("").reverse().join("");
+    console.log(polishedString)
+    if (polishedString != reversedString){
+      return false;
+    }
+    return true;
+  }
+  
+  
+  let result = palindrome("_eye")
+  console.log(result);
